8d00f43d19dbb500f3341b9669e31c915828d032
Robertoconnor94@gmail.com <Robertoconnor94@gmail.com>
2025-05-04 13:21:42 +0100

    Added annoying popup.

---
+++ Assets/Scripts/AnnoyingPopup.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class AnnoyingPopup : MonoBehaviour
+{
+
+    public static string[] genericTitles = {
+        "Win a free car!",
+        "Free holiday to Spain!",
+        "You are the winner of $1,000,000!",
+        "Free beans for life!",
+        "You won a new blender!",
+        "Update your will for free!",
+        "Singles in your area!",
+        "Singles are waiting for you!",
+        "Lifetime supply of easy singles!",
+    };
+
+
+    public Image bgImage;
+    public TMP_Text title;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        title.text = genericTitles[Random.Range(0, genericTitles.Length)];
+
+        title.color = randomTextColor();
+        bgImage.color = randomBGColor();
+
+        LeanTween.scale(title.gameObject, Vector3.one * 1.3f, 0.4f)
+        .setEaseInOutSine()
+        .setLoopPingPong();
+
+        LeanTween.rotateZ(title.gameObject, 30f, 0.5f)
+        .setEaseInOutSine()
+        .setLoopPingPong();
+    }
+
+/*    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<RectTransform>().anchoredPosition = new Vector2(Random.Range(-450, 450), Random.Range(-250, 250));
+
+            Start();
+        }
+    }*/
+
+
+    Color randomBGColor()
+    {
+        return new Color(Random.Range(100, 255f) / 255f, Random.Range(100, 255f) / 255f, Random.Range(100, 255f) / 255f);
+    }
+
+    Color randomTextColor()
+    {
+        return new Color(Random.Range(0, 120f) / 255f, Random.Range(0, 120f) / 255f, Random.Range(0, 120f) / 255f);
+    }
+}
